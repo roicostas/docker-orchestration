af58e3078d43688d0ddd1fc5b26f5706f308c1df
Roi Costas <roi.costas@torusware.com>
2016-06-15 11:22:24 +0200

    Avoid incrementing hash count when there is an error

--- dockercoins/worker/worker.py
+++ dockercoins/worker/worker.py
@@ -19,8 +19,10 @@ redis = Redis("redis")
 
 def get_random_bytes():
     r = requests.get("http://rng/32")
-    return r.content
-
+    if r.status_code == 200:
+        return r.content
+    else:
+        return False
 
 def hash_bytes(data):
     r = requests.post("http://hasher/",
@@ -40,23 +42,25 @@ def work_loop(interval=1):
             redis.incrby("hashes", loops_done)
             loops_done = 0
             deadline = time.time() + interval
-        work_once()
-        loops_done += 1
+        if work_once():
+            loops_done += 1
 
 
 def work_once():
     log.debug("Doing one unit of work")
     time.sleep(0.1)
     random_bytes = get_random_bytes()
+    if not random_bytes:
+        return False
     hex_hash = hash_bytes(random_bytes)
     if not hex_hash.startswith('0'):
         log.debug("No coin found")
-        return
+        return True
     log.info("Coin found: {}...".format(hex_hash[:8]))
     created = redis.hset("wallet", hex_hash, random_bytes)
     if not created:
         log.info("We already had that coin")
-
+    return True
 
 if __name__ == "__main__":
     while True:
