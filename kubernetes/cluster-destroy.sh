cb2bb38484012ae9e1614c1d090b7b53c0ed5773
roi.costas <roi.costas@torusware.com>
2016-08-24 15:22:39 +0200

    Add cluster destroy script

---
+++ kubernetes/cluster-destroy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+BASE_PWD=$PWD
+cd coreos-kubernetes/multi-node/vagrant
+vagrant destroy -f
+
+cd $BASE_PWD
+rm -rf coreos-kubernetes
+rm key key.pub
+rm kubeconfig
+rm kubectl
+rm dockercoins.env
