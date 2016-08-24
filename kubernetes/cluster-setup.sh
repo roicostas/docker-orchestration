34151979ecf1d17b7bafecdcdac2c9b76d6918a9
roi.costas <roi.costas@torusware.com>
2016-08-24 15:17:51 +0200

    Fix cluster enviroment setup

--- kubernetes/cluster-setup.sh
+++ kubernetes/cluster-setup.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 
 BASE_PWD=$PWD
+rm dockercoins.env
 
-# Install kubectl, kubernetes client
-if ! type kubectl &>/dev/null ; then
-    echo -e "Error: kubectl not installed, install it with
-        curl -O https://storage.googleapis.com/kubernetes-release/release/v1.3.4/bin/linux/amd64/kubectl
-        chmod +x kubectl
-        sudo mv kubectl /usr/local/bin/kubectl"
-    exit 1
-fi
-
-# Setup CoreOS-kubernetes cluster
-git clone https://github.com/coreos/coreos-kubernetes.git
+# -- Setup CoreOS-kubernetes cluster --
+#git clone https://github.com/coreos/coreos-kubernetes.git
 cd coreos-kubernetes/multi-node/vagrant
 
 # Condigure two controllers
-sed '/s/$controller_count=1/$controller_count=2/g' < config.rb.sample > config.rb
+#sed 's/#$controller_count=1/$controller_count=2/g' < config.rb.sample > config.rb
 
 # Create cluster
-vagrant up
+#vagrant up
 
-# Configure kubectl
+# -- Configure kubernetes client kubectl --
+curl -O https://storage.googleapis.com/kubernetes-release/release/v1.3.4/bin/linux/amd64/kubectl > $BASE_PWD/kubectl
+chmod +x $BASE_PWD/kubectl
+echo 'export PATH=$PATH:$BASE_PWD' > $BASE_PWD/dockercoins.env
+export PATH=$PATH:$BASE_PWD
+
+echo 'export KUBECONFIG="${KUBECONFIG}:$PWD/kubeconfig"' >> $BASE_PWD/dockercoins.env
 export KUBECONFIG="${KUBECONFIG}:$PWD/kubeconfig"
 kubectl config set-cluster vagrant-multi-cluster \
     --server=https://172.17.4.101:443 \
@@ -35,18 +33,25 @@ kubectl config set-context vagrant-multi \
     --user=vagrant-multi-admin
 kubectl config use-context vagrant-multi
 
-# Allow scheduling in master nodes
+# -- Configure ssh authentication --
+ssh-keygen -f $BASE_PWD/key -t rsa -N ""
+for node in $(vagrant status | grep running | awk '{print $1}'); do
+    cat $BASE_PWD/key.pub | vagrant ssh -c "mkdir -p .ssh && cat ->> .ssh/authorized_keys" $node 2>/dev/null
+    node_ip=$(vagrant ssh -c "ip address show eth1 | grep 'inet ' | sed -e 's/^.*inet //' -e 's/\/.*$//'" $node | tr -d '\r')
+    echo export $node=$node_ip >> $BASE_PWD/dockercoins.env
+    echo $node=$node_ip
+done
+echo Enviroment information saved at dockercoins.env
+
+cd $BASE_PWD
+. $BASE_PWD/dockercoins.env
+# Wait for kubernetes cluster to be up
+while ! curl $c1:443 ; do
+    echo Waiting for kubernetes to be up
+    sleep 1
+done
+
+# -- Allow scheduling in master nodes --
 for node in $(kubectl get nodes --no-headers | grep SchedulingDisabled | awk '{print $1}'); do
     kubectl uncordon $node
 done
-
-# Configure ssh authentication
-ssh-keygen -f $BASE_PWD/key -t rsa -N ""
-rm $BASE_PWD/nodes.txt
-for node in $(vagrant status | grep running | awk '{print $1}'); do
-    cat $BASE_PWD/key.pub | vagrant ssh -c "mkdir -p .ssh && cat ->> .ssh/authorized_keys" $node 2>/dev/null
-    node_ip=$(vagrant ssh -c "ip address show eth1 | grep 'inet ' | sed -e 's/^.*inet //' -e 's/\/.*$//'" $node)
-    echo $node=$node_ip | tee -a $BASE_PWD/nodes.env
-done
-echo Cluster nodes saved to nodes.env
-cd $BASE_PWD
