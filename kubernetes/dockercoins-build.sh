34d9af0a3eaf939492e8f905a0a7771d9aed7856
roi.costas <roi.costas@torusware.com>
2016-08-24 15:17:01 +0200

    Use registry in dockercoins.env for kubernetes registry

--- kubernetes/dockercoins-build.sh
+++ kubernetes/dockercoins-build.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-. nodes.env
 . dockercoins.env
 
 # Use docker-compose to build and push the application to local registry
-#ssh -i key core@$w1 <<EOF
-#git clone https://github.com/roicostas/docker-orchestration
-#cd docker-orchestration/dockercoins
-#curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > docker-compose
-#chmod +x docker-compose
-export REGISTRY=172.17.4.201:"$REGISTRY_PORT"
-export VERSION=$VERSION
-docker-compose -f ../dockercoins/docker-compose.yml-registry build
-docker-compose -f ../dockercoins/docker-compose.yml-registry push
-#EOF
+ssh -i key core@$w1 <<EOF
+rm -rf docker-orchestration
+git clone https://github.com/roicostas/docker-orchestration
+cd docker-orchestration/dockercoins
+curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > docker-compose
+chmod +x docker-compose
+export REGISTRY_DASH=$REGISTRY_DASH
+./docker-compose -f docker-compose.yml-registry build
+./docker-compose -f docker-compose.yml-registry push
+EOF
