d8d1cf6380e79b0c464a19a33a58dc7209cce0b8
roi.costas <roi.costas@torusware.com>
2016-08-30 08:20:56 +0200

    Redirect local regsitry port to kubernetes

--- kubernetes/dockercoins-build.sh
+++ kubernetes/dockercoins-build.sh
@@ -2,16 +2,9 @@
 
 . dockercoins.env
 
-# Use docker-compose to build and push the application to local registry
-ssh -i key core@$w1 <<EOF
-rm -rf docker-orchestration
-git clone https://github.com/roicostas/docker-orchestration
-cd docker-orchestration/dockercoins
-curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > docker-compose
-chmod +x docker-compose
-export REGISTRY_DASH=$REGISTRY_DASH
-./docker-compose -f docker-compose.yml-registry build
-./docker-compose -f docker-compose.yml-registry push
-docker tag ${REGISTRY_DASH}dockercoins_worker ${REGISTRY_DASH}dockercoins_worker:v2
-docker push ${REGISTRY_DASH}dockercoins_worker:v2
-EOF
+# Redirect registry url localhost:5000 to kubernetes registry service
+socat TCP-LISTEN:5000,fork TCP:$c1:$REGISTRY_PORT &
+
+export REGISTRY_DASH=localhost:5000/
+docker-compose -f ../dockercoins/docker-compose.yml-registry build
+docker-compose -f ../dockercoins/docker-compose.yml-registry push
