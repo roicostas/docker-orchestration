f9beb153258bb2a590e24c2e3884bfe0d005a857
roi.costas <roi.costas@torusware.com>
2016-08-30 08:21:40 +0200

    Create new version of worker

--- kubernetes/dockercoins-upgrade.sh
+++ kubernetes/dockercoins-upgrade.sh
@@ -2,12 +2,12 @@
 
 . dockercoins.env
 
+# Create new dockercoins_worker version, use port forwarding again to reach kubernetes registry
+docker tag localhost:5000/dockercoins_worker localhost:5000/dockercoins_worker:v2
+docker push localhost:5000/dockercoins_worker:v2
+
 # Update worker image
 kubectl set image deployment/worker worker=${REGISTRY}/dockercoins_worker:v2
 
 # Monitor update process
 kubectl rollout status deployment/worker
-
-kubectl get deployment worker
-
-kubectl describe deployment worker
