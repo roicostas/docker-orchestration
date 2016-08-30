b6f1b49d7b61aafda5c665046d7cd5840b1d1f23
roi.costas <roi.costas@torusware.com>
2016-08-30 08:26:37 +0200

    Do not add new version worker at this point

--- swarm/dockercoins-build.sh
+++ swarm/dockercoins-build.sh
@@ -2,9 +2,7 @@
 
 eval $(docker-machine env node-1)
 
-# Build and push images to a remote registry through an ambassador
+# Build and push images with docker compose
 export REGISTRY_DASH=localhost:5000/
 docker-compose -f ../dockercoins/docker-compose.yml-registry build
 docker-compose -f ../dockercoins/docker-compose.yml-registry push
-docker tag localhost:5000/dockercoins_worker localhost:5000/dockercoins_worker:v2
-docker push localhost:5000/dockercoins_worker:v2
